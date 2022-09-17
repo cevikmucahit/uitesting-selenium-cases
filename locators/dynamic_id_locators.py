20a4b3d288b811bfbaa1cbf319cb441cecb008c7
mücahit çevik <mucahittcevikk@hotmail.com>
2022-09-17 18:03:57 +0300

    bug fixed

--- locators/dynamic_id_locators.py
+++ locators/dynamic_id_locators.py
@@ -1,4 +1,4 @@
 from selenium.webdriver.common.by import By
 
-DYNAMIC_URL = "http://uitestingplayground.com/dynamicidd"
+DYNAMIC_URL = "http://uitestingplayground.com/dynamicid"
 BUTTON = (By.CSS_SELECTOR, ".container button")
